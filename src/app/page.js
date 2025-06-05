0aec6d4bc309514f512eb07a0c0c1a9d74b6cd4a
manish-gkv <manishkumarslmp@gmail.com>
2025-06-06 00:12:51 +0530

    deployment-commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import MiddleSection from "@/components/MiddleSection";
 import ImageSection from "@/components/ImageSection";
 import SocialMediaSection from "@/components/SocialMediaSection";
 
-// This is the main entry point for the home page of the application.
+// This is the main entry point for the home page of the application
 export default function Home() {
   return (
     <div className="px-4 sm:px-6 md:px-50 lg:px-50 xl:px-100 2xl:px-100 pb-20 text-base sm:text-lg">
